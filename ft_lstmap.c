99728969411fe100836fbd12d3a927330207f9c4
hoaithu3868 <thninh@student.42.fr>
2016-11-17 01:07:43 +0100

    Update ft_lstmap.c

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -11,3 +11,38 @@
 /* ************************************************************************** */
 
 
+#include "libft.h"
+static size_t   ft_lst_len(t_list *lst)
+{
+	size_t	len;
+
+	len = 0;
+	while (lst)
+	{
+		len++;
+		lst = lst->next;
+	}
+	return (len);
+}
+
+t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+{
+	t_list	*tmp_new_map;
+	t_list	*new_map;
+
+	new_map = NULL;
+	tmp_new_map = NULL;
+	if ((new_map = (t_list *)malloc(ft_lst_len(lst) * sizeof(t_list))))
+	{
+		new_map = f(ft_lstnew(lst->content, lst->content_size));
+		tmp_new_map = new_map;
+		lst = lst->next;
+		while (lst)
+		{
+			tmp_new_map->next = f(ft_lstnew(lst->content, lst->content_size));
+			tmp_new_map = tmp_new_map->next;
+			lst = lst->next;
+		}
+	}
+	return (new_map);
+}
