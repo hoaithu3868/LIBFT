04234e2d4dc56a7b8895aa0768f8621735f46030
hoaithu3868 <thninh@student.42.fr>
2016-11-17 01:01:00 +0100

    Update ft_lstdel.c

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -10,4 +10,24 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void   ft_lstdel(t_list **alst, void (*del)(void *, size_t))
+{
+  t_list  *list;
+  t_list  *nxt;
+
+  list = *alst;
+  if (list)
+  {
+    while (list->next)
+    {
+      nxt = list->next;
+      del(list->content, list->content_size);
+      free(list)
+      list = list->next;
+    }
+    list = NULL;
+  }
+}
 
