c575e776c18380ccebac7b5400af4d580c1249e4
hoaithu3868 <thninh@student.42.fr>
2016-11-17 01:05:07 +0100

    Update ft_lstiter.c

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -10,4 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void    ft_lstiter(t_list *lst, void (*f)(t_list *elem))
+{
+  if (lst && f)
+  {
+    f(lst);
+    lst = lst->next;
+  }
+}
