b8f3c1c41fcf2bb974de79b94876d88c6d748fbc
hoaithu3868 <thninh@student.42.fr>
2016-11-17 01:02:27 +0100

    Update ft_lstdelone.c

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -10,4 +10,17 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void    ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
+{
+  t_list *list;
+
+  list = *alst;
+  if (list)
+  {
+    del(list->content, list->content_size);
+    free(*list);
+    *list = NULL;
+  }
+}
